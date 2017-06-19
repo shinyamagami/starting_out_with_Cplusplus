b8a9ce10d671490b59495c709a732da78adf2eff
Shin Yamagami <kiboh.usa@gmail.com>
2017-06-19 00:14:01 -0600

    Upload

---
+++ Chapter.02/01/sum_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a = 50;
+   int b = 100;
+
+   int total = a + b;
+
+   cout << total;
+   return 0;
+}
---
+++ Chapter.02/02/sales_prediction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   double ratio = 0.58;
+   int sales = 8600000;
+
+   int total = sales * ratio;
+
+   cout << total;
+   return 0;
+}
